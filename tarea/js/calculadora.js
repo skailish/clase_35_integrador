5c90e26a6a6e22c0673787ea2446182edfdd17e7
Daniela Capponi <capponi.daniela@gmail.com>
2020-02-07 20:44:50 -0300

    calculadora switch

--- tarea/js/calculadora.js
+++ tarea/js/calculadora.js
@@ -1,36 +1,36 @@
 // Calculadora
 
-// Crear un programa que permita elegir entre las operaciones: SUMA, RESTA, DIVISION y MULTIPLICACION, y luego de elegida, ingresar dos números y hacer dicha operación con ella.
+// Crear un programa que permita elegir entre las operaciones: SUMA, RESTA, DIVISION y MULTIPLICACION, y luego de elegida, ingresar dos números y hacer dicha operación con ella. Si se ingresa una opción incorrecta debe mostrar un mensaje de error.
+
 
 let value1 = 0;
 let value2 = 0;
-let result = 0;
 let operation = "";
 
+// DEFINE OPERACIÓN
 operation = prompt(`
 Elija una operación:
-1 - sumar
-2 - restar
-3 - dividir
-4 - multiplicar`);
-
+sumar
+restar
+dividir
+multiplicar`);
+// DEFINE VALORES
 value1 = Number(prompt("Elija el valor 1"));
 value2 = Number(prompt("Elija el valor 2"));
 
-if (operation == 1) { // SUMAR
-    result = value1 + value2;
-
-} else if (operation == 2) { // RESTAR
-    result = value1 - value2;
-
-} else if (operation == 3) { // DIVIDIR
-    result = value1 / value2;
-
-} else if (operation == 4) { // MULTIPLICAR
-    result = value1 * value2;
-
-} else { // ERROR
-    alert("No ingresó una opción correcta");
+switch (operation) {
+    case "sumar":
+        alert(`El resultado es ${(value1 + value2)}`)
+        break;
+    case "restar":
+        alert(`El resultado es ${(value1 - value2)}`)
+        break;
+    case "dividir":
+        alert(`El resultado es ${(value1 / value2)}`)
+        break;
+    case "multiplicar":
+        alert(`El resultado es ${(value1 * value2)}`)
+        break;
+    default:
+        alert("Ingrese una opción válida")
 }
-
-alert("El resultado es " + result);
